ca476316ef855d2f5f8a35a6c13e64dbb99b28c4
ameetgill <ameetpalsingh@gmail.com>
2020-08-01 04:45:36 +0000

    added serial writer

--- src/serial.rs
+++ src/serial.rs
@@ -1,19 +1,19 @@
 use uart_16550::SerialPort;
 use spin::Mutex;
-use lazy_static::LazyStatic;
+use lazy_static::lazy_static;
 
-lazy_static{
+lazy_static! {
     pub static ref SERIAL1: Mutex<SerialPort> = {
-        let mut serial_port = unsafe { SerialPort::new(0xf4) };
+        let mut serial_port = unsafe { SerialPort::new(0x3F8) };
         serial_port.init();
         Mutex::new(serial_port)
-    }
-};
+    };
+}
 
 #[doc(hidder)]
 pub fn _print(arg: ::core::fmt::Arguments) {
     use core::fmt::Write;
-    SERIAL1.lock().write_fmt(arg).unwrap();
+    SERIAL1.lock().write_fmt(arg).expect("Printing to serial failed");
 }
 
 #[macro_export]
@@ -23,9 +23,10 @@ macro_rules! serial_print {
     };
 }
 
-#[macro_exports]
-macro_rules!  {
-    () => $crate::serial_print!("\n");
-    ($fmt:expr) => ($crate::serial_print!(concat!($fmt,"\n")));
-    ($fmt:expr, $($arg:tt)*) => ($crate::serial_print!(concat!($fmt, "\n"), $($arg)*));   
+#[macro_export]
+macro_rules! serial_println {
+    () => ($crate::serial_print!("\n"));
+    ($fmt:expr) => ($crate::serial_print!(concat!($fmt, "\n")));
+    ($fmt:expr, $($arg:tt)*) => ($crate::serial_print!(
+        concat!($fmt, "\n"), $($arg)*));
 }
